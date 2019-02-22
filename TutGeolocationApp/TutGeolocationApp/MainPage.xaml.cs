6c41276fbfd1974eb98b699feda061a122349dba
David O <davideoguche@gmail.com>
2019-02-22 22:21:53 +0100

    Geolocation Setup

--- TutGeolocationApp/TutGeolocationApp/MainPage.xaml.cs
+++ TutGeolocationApp/TutGeolocationApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace TutGeolocationApp
 {
@@ -12,6 +13,73 @@ namespace TutGeolocationApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private Location myLocation;
+
+        private async void ButtonGetCurrentLoc_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.High);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    labelLongLat.Text = "Lat: " + location.Latitude + " Long: " + location.Longitude;
+                    myLocation = location;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        private async void ButtonConvert_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var placemarks = await Geocoding.GetPlacemarksAsync(myLocation.Latitude, myLocation.Longitude);
+
+                var placemark = placemarks?.FirstOrDefault();
+                if (placemark != null)
+                {
+                    var geocodeAddress =
+                        $"AdminArea:       {placemark.AdminArea}\n" +
+                        $"CountryCode:     {placemark.CountryCode}\n" +
+                        $"CountryName:     {placemark.CountryName}\n" +
+                        $"FeatureName:     {placemark.FeatureName}\n" +
+                        $"Locality:        {placemark.Locality}\n" +
+                        $"PostalCode:      {placemark.PostalCode}\n" +
+                        $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                        $"SubLocality:     {placemark.SubLocality}\n" +
+                        $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                        $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    Console.WriteLine(geocodeAddress);
+                    labelAddress.Text = geocodeAddress;
+                }
+            } catch (Exception exception)
+            {
+
+            }
+        }
+
+        private void ButtonCompareDistance_Clicked(object sender, EventArgs e)
+        {
+            Location sanFrancisco = new Location(37.783333, -122.416667);
+            double miles = Location.CalculateDistance(myLocation, sanFrancisco, DistanceUnits.Miles);
+            labelDistance.Text = miles + "Miles";
         }
     }
 }
